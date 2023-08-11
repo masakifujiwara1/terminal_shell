750414aec31aa222cc41c86aed8442ea4841ad56
MasakiFujiwara <fmasaki2020@gmail.com>
2023-08-11 17:40:56 +0900

    fix for imu launch

--- start_with_bridge.sh
+++ start_with_bridge.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 gnome-terminal -- bash -c 'source /opt/ros/noetic/setup.bash && roscore; exec bash'
 gnome-terminal -- bash -c 'source /opt/ros/noetic/setup.bash && source /opt/ros/foxy/setup.bash && source ~/ros2_ws/install/setup.bash && ros2 run ros1_bridge dynamic_bridge; exec bash'
-gnome-terminal -- bash -c 'source /opt/ros/noetic/setup.bash && source ~/catkin_ws/devel/setup.bash && roslaunch rfans_driver node_manager.launch; exec bash'
+gnome-terminal -- bash -c 'source /opt/ros/noetic/setup.bash && source ~/catkin_ws/devel/setup.bash && roslaunch adi_driver adis16470.launch; exec bash'
